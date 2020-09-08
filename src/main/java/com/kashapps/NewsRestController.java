b8900d4379459dc0ec2d37b4abe1f10ccc32594d
Ankur Kumar <ankur.kumar@landisgyr.com>
2020-09-08 16:21:53 +0530

    Identification: dummy first commit
    Change-Id: I31a45b877f06568ec307f4ffecc42a03fb5e69be

---
+++ src/main/java/com/kashapps/NewsRestController.java
@@ -0,0 +1,13 @@
+package com.kashapps;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewsRestController {
+
+    @RequestMapping("/message")
+    public String getMessage() {
+        return "Hello News Scrapper";
+    }
+}
---
+++ src/main/java/com/kashapps/NewsScrapperMain.java
@@ -0,0 +1,11 @@
+package com.kashapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsScrapperMain {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsScrapperMain.class, args);
+    }
+}
